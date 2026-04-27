e2dd83d1338431da68ccc1497afd5f0f25744191
Andrew R <Mydrewdog64@gmail.com>
2026-04-26 20:33:44 -0400

    Initial Commit

---
+++ Main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var xr_interface: XRInterface
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	xr_interface = XRServer.find_interface("OpenXR")
+
+	if xr_interface and xr_interface.is_initialized():
+		print("OPEN XR INITIALIZED SUCCESSFULLY")
+
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+		get_viewport().use_xr = true
+	else:
+		print("OPENXR Not Initialized please check if the headset is connected")
